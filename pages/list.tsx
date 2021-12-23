9fb3b19a94eb08564e4807390a33a313579af43d
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2021-12-23 15:00:27 +0300

    Create list page for state display

---
+++ pages/list.tsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { Layout } from "../components/layouts";
+import { TodoItem } from "../components/todoItem";
+import { AppState } from "../helpers/store";
+
+export default function List() {
+  const TodoList = useSelector((state: AppState) => state.todo);
+
+  return (
+    <Layout variant="home">
+      <div className="w-full h-screen">
+        <div className="my-48 min-h-min h-8 py-24 ">
+          <div className="space-y-2">
+            <div className="rounded text-gray-800 max-w-md mx-auto flex justify-between px-4 w-full">
+              <div>{"Sırada Ne var "}</div>
+              <div> {"Kaç Kez"}</div>
+            </div>
+            {TodoList.item.map((item, index) => (
+              <TodoItem
+                index={index}
+                key={index}
+                text={item.toDoText}
+                howMany={item.howMany}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}

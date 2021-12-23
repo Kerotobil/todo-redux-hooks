e7a6bce17f0b88b38d49a15352de60386744682f
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2021-12-23 11:30:47 +0300

    Install redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import rootReducer from "../helpers/store";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
-
-export default MyApp
+export default MyApp;
